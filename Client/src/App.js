db0a6454d79cdce1b3e102262877e338de919ecd
mamdouh <mamdouh>
2022-03-06 15:03:53 +0200

    adding header coponent

---
+++ Client/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./components/Header/Header";
+
+
+
+function App() {
+  return (
+    <div className="layout">
+      <Header />
+      <main>
+        Conent
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../css/Header/Header.css';
+export default function () {
+    return (
+        <>
+            <header>
+                Shoping Cart
+            </header>
+        </>
+    )
+}

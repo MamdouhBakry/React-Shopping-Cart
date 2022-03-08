128e9f1f7eb5d649dc481b0972e2f14651464891
mamdouh <mamdouh>
2022-03-08 16:23:13 +0200

    animation part2

--- Client/src/components/CheckoutForm/CheckoutForm.js
+++ Client/src/components/CheckoutForm/CheckoutForm.js
@@ -1,30 +1,39 @@
 import React from 'react'
 import "../../css/CheckoutForm/CheckoutForm.css";
 import Input from '../Input/Input';
+import Zoom from 'react-reveal/Zoom';
+import LightSpeed from 'react-reveal/LightSpeed';
+
 export default function CheckoutForm(props) {
     return (
-        <>
+        < >
+
             {
-                props.showForm && <div className="checkout-form">
-                    <span onClick={() => props.setShowForm(false)} className="close-icon">&times;</span>
-                    <form onSubmit={props.submitOrder}>
-                        <Input
-                            label="Name"
-                            type="text"
-                            onChange={props.handleChange}
-                            name="name"
-                        />
-                        <Input
-                            label="Email"
-                            type="email"
-                            onChange={props.handleChange}
-                            name="email"
-                        />
-                        <div>
-                            <button type="submit">Checkout</button>
-                        </div>
-                    </form>
-                </div>
+                props.showForm &&
+                <LightSpeed right>
+                    <div className="checkout-form">
+                        <span onClick={() => props.setShowForm(false)} className="close-icon">&times;</span>
+                        <Zoom right>
+                            <form onSubmit={props.submitOrder}>
+                                <Input
+                                    label="Name"
+                                    type="text"
+                                    onChange={props.handleChange}
+                                    name="name"
+                                />
+                                <Input
+                                    label="Email"
+                                    type="email"
+                                    onChange={props.handleChange}
+                                    name="email"
+                                />
+                                <div>
+                                    <button type="submit">Checkout</button>
+                                </div>
+                            </form>
+                        </Zoom>
+                    </div>
+                </LightSpeed>
             }
         </>
     )
--- Client/src/components/Filter/Filter.js
+++ Client/src/components/Filter/Filter.js
@@ -1,34 +1,38 @@
 import React from 'react';
 import "../../css/Filter/Filter.css";
+import Flip from 'react-reveal/Flip';
+
 export default function Filter(props) {
     return (
         <>
-            <div className="filter-wrapper">
-                <div className="container">
-                    <h2 className="filter-title">Filter</h2>
-                    <div className="num-of-products">Number of Products ({props.productsNum})</div>
-                    <div className="filter-by-size">
-                        <span>Filter</span>
-                        <select value={props.size} onChange={props.handleFilterBySize} className="filter-select">
-                            <option value="ALL">ALL</option>
-                            <option value="XS">XS</option>
-                            <option value="S">S</option>
-                            <option value="M">M</option>
-                            <option value="L">L</option>
-                            <option value="XL">XL</option>
-                        </select>
+            <Flip left>
+                <div className="filter-wrapper">
+                    <div className="container">
+                        <h2 className="filter-title">Filter</h2>
+                        <div className="num-of-products">Number of Products ({props.productsNum})</div>
+                        <div className="filter-by-size">
+                            <span>Filter</span>
+                            <select value={props.size} onChange={props.handleFilterBySize} className="filter-select">
+                                <option value="ALL">ALL</option>
+                                <option value="XS">XS</option>
+                                <option value="S">S</option>
+                                <option value="M">M</option>
+                                <option value="L">L</option>
+                                <option value="XL">XL</option>
+                            </select>
+                        </div>
+                        <div className="filter-by-size">
+                            <span>Order</span>
+                            <select value={props.sort} onChange={props.handleFilterByOrder} className="filter-select">
+                                <option value="latest">Latest</option>
+                                <option value="lower">Lower</option>
+                                <option value="highest">Highest</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="filter-by-size">
-                        <span>Order</span>
-                        <select value={props.sort} onChange={props.handleFilterByOrder} className="filter-select">
-                            <option value="latest">Latest</option>
-                            <option value="lower">Lower</option>
-                            <option value="highest">Highest</option>
-                        </select>
-                    </div>
-                </div>
 
-            </div>
+                </div>
+            </Flip>
         </>
     )
 }

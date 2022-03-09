e4b83b4cc5cc0c21d1a014547177f83a35c9713e
mamdouh <mamdouh>
2022-03-09 19:27:48 +0200

    product model schema and routes file

---
+++ Server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const env = require('dotenv')
+const Product = require('./models/ProductModel');
+const router = require('./routes/routes');
+const app = express();
+
+app.use(bodyParser.json());
+app.use("/", router);
+
+mongoose
+    .connect(
+        `mongodb+srv://root:admin@cluster0.ipjag.mongodb.net/shoping_cart?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log("Database Connected");
+    });
+
+
+
+const port = process.env.PORT || 5001;
+const start = async () => {
+    try {
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();

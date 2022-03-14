d8ea0dcb05838a73b98c55b016026a670b96fc40
mamdouh <mamdouh>
2022-03-14 16:07:25 +0200

    add react router

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,9 +1,7 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Products from "./components/Products/Products";
-import Filter from "./components/Filter/Filter";
-import Cart from "./components/Cart/Cart";
 import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Images
 import img1 from "./Images/airePods.jpg";
@@ -11,25 +9,29 @@ import img2 from "./Images/camera.jpg";
 import img3 from "./Images/laptop.jpg";
 import img4 from "./Images/shoes.jpg";
 import store from "./store/store";
+import Home from "./pages/Home";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar/Navbar";
 
 
 function App() {
   let images = [img1, img2, img3, img4];
   return (
-    <Provider store={store}>
-      <div className="layout">
-        <Header />
-        <main>
-          <div className="wrapper">
-            <Products images={images} />
-            <Filter
-            />
-          </div>
-          <Cart images={images} />
-        </main>
-        <Footer />
-      </div>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <div className="layout">
+          <Header />
+          <main>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home images={images} />} exact />
+              <Route path="/orders" element={<Orders images={images} />} exact />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
